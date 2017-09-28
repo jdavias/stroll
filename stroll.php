384e6f9ca89dd0ab8d8c5c83e1a4b9248445ff86
Jean Davias <jdavias@gmail.com>
2017-09-28 16:39:07 +0400

    Introduction classes

--- stroll.php
+++ stroll.php
@@ -1,55 +1,84 @@
 <?php
 
-$letters = 'acdegilmnoprstuw';
+class Hasher {
 
-$target = '945924806726376';
+    private $letters;
+    private $base;
 
-$max = pow(16, 9) - 1;
-
-$hash = function($string) use ($letters) {
-    $h = 7;
-    for ($i = 0; $i < strlen($string) ; $i++) {
-        $h = $h * 37 + strpos($letters, $string[$i]);
-    }
-    return $h;
-};
-
-$hex2alpha = function($number) use ($letters) {
-    $string = '';
-    for ($i = 0; $i < strlen($number); $i++) {
-        $string .= $letters[hexdec($number[$i])];
-    }
-    return $string;
-};
-
-$binarySearch = function($target, $left, $right) use ($hash, $hex2alpha, &$binarySearch) {
-
-    echo PHP_EOL . 'L:' . $left . ' R:' . $right;
-
-    if ($left > $right) {
-        return false;
+    public function __construct($letters) {
+        $this->letters = $letters;
+        $this->base = strlen($this->letters);
     }
 
-    $middle = floor(($left + $right) / 2);
-    $alphaMiddle = $hex2alpha(dechex($middle));
-    $hashedMiddle = $hash($alphaMiddle);
-
-    if ($hashedMiddle == $target) {
-        return $alphaMiddle;
+    public function letters() {
+        return $this->letters;
     }
-    elseif ($hashedMiddle > $target) {
-        return $binarySearch($target, $left, $middle - 1);
-    }
-    else {
-        return $binarySearch($target, $middle + 1, $right);
-    }
-};
 
-$result = $binarySearch($target, 0, $max);
+   /**
+    * Returns the n-th arrangement of letters from $letters.
+    *
+    * The decimal rank is converted to the base which radix is equal to the length of $letters.
+    * Each character of the string representation of the converted rank is then converted back
+    * to 10-base.
+    */
+    public function nthWord($rank) {
 
-echo PHP_EOL . $result;
-if ($hash($result) == $target) {
-    echo PHP_EOL . 'CORRECT!';
+        $number = base_convert($rank, 10, $this->base);
+
+        $string = '';
+        for ($i = 0; $i < strlen($number); $i++) {
+            $string .= $this->letters[base_convert($number[$i], $this->base, 10)];
+        }
+        return $string;
+    }
+
+    public function hash($word) {
+        $h = 7;
+        for ($i = 0; $i < strlen($word) ; $i++) {
+            $h = $h * 37 + strpos($this->letters, $word[$i]);
+        }
+        return $h;
+    }
 }
+
+class HashBinarySearch {
+
+    private $hasher;
+
+    public function __construct(Hasher $hasher) {
+        $this->hasher = $hasher;
+    }
+
+    public function find($target) {
+        $max = pow(strlen($this->hasher->letters()), strlen($target)) - 1;
+        return $this->search($target, 0, $max);
+    }
+
+    public function search($target, $left, $right) {
+
+        if ($left > $right) {
+            return false;
+        }
+
+        $middle = floor(($left + $right) / 2);
+        $alphaMiddle = $this->hasher->nthWord($middle);
+        $hashedMiddle = $this->hasher->hash($alphaMiddle);
+
+        if ($hashedMiddle == $target) {
+            return $alphaMiddle;
+        }
+        elseif ($hashedMiddle > $target) {
+            return $this->search($target, $left, $middle - 1);
+        }
+        else {
+            return $this->search($target, $middle + 1, $right);
+        }
+    }
+}
+
+$search = new HashBinarySearch(new Hasher('acdegilmnoprstuw'));
+
+echo $search->find('945924806726376');
+echo PHP_EOL;
 
 ?>
